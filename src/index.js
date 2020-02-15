30fb29cbb2272909b55b3fe2463b80e54d664735
Heronnes Pereira <heronnestg@hotmail.com>
2020-02-15 12:11:39 -0300

    parte 9

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios'   //global
+
+axios.interceptors.request.use(requesicao => {
+    console.log(requesicao);
+    // Edit request config
+    return requesicao
+ }, erro => {
+     console.log(erro)
+     return Promise.reject(erro)
+ })
+
+ axios.interceptors.response.use(resposta => {
+    console.log(resposta)
+    //Edit request config
+    return resposta
+ }, erro => {
+    console.log(erro)
+    return Promise.reject(erro)
+ })
+
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
