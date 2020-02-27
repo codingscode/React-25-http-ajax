3f1bd899b0480559c5da6fbb4f4f4dd27bef5ab4
Heronnes Pereira <heronnestg@hotmail.com>
2020-02-27 17:27:31 -0300

    parte 16

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -17,8 +17,9 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-            <Route path="/" exact render={() => <h1>Home</h1>} />  
-            <Route path="/" render={() => <h1>Home 2</h1>} />  
+            {/*<Route path="/" exact render={() => <h1>Home</h1>} />  
+            <Route path="/" render={() => <h1>Home 2</h1>} />*/} 
+            <Route path="/" exact component={Posts} /> 
             </div>
         );
     }
