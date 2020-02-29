cfd197154211a69646cf79b3c36a2793165f7a05
Heronnes Pereira <heronnestg@hotmail.com>
2020-02-29 13:07:41 -0300

    parte 21

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import {withRouter} from 'react-router-dom'
 
 import './Post.css';
 
-const post = (props) => {      // transforma em componente
-    console.log(props)
-
-    return (
-        <article className="Post" onClick={props.clicado}>
+const post = (props) => (
+       <article className="Post" onClick={props.clicado}>
             <h1>{props.titulo}</h1>
             <div className="Informacao">
                 <div className="Autor">{props.autor}</div>
             </div>
         </article>
-    )
     
-};
+);
 
-export default withRouter(post);
+export default post
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Route, Link} from 'react-router-dom'
+import {Route, NavLink} from 'react-router-dom'
 
 import './Blog.css';
 import Posts from '../../components/Posts/Posts'
@@ -13,12 +13,15 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/" >Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink to="/" exact activeClassName="meu-ativo" activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Home</NavLink></li>
+                            <li><NavLink to={{      // o exact aqui não é necessário
                                 pathname: '/novo-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                                }} >Novo Post</Link></li>
+                                }} >Novo Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
